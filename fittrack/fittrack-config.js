76c6294b430fa5a094906d40e070ff98e6596471
sanrique <sanrique2400@gmail.com>
2026-05-26 08:54:39 -0300

    deploy fittrack: fittrack-config.js

---
+++ fittrack/fittrack-config.js
@@ -0,0 +1,8 @@
+window.FITTRACK_FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyA0hYRBZB7a6fYVV2_TXlkLxPbgkWNZUhI",
+  authDomain:        "fittrack-33b53.firebaseapp.com",
+  projectId:         "fittrack-33b53",
+  storageBucket:     "fittrack-33b53.firebasestorage.app",
+  messagingSenderId: "202823507242",
+  appId:             "1:202823507242:web:60fa1748d62114f2c7d253",
+};
